21c9c475444908d175ffa61004e2c6ec36ab4646
George Monibah <monibahgeorgew@gmail.com>
2025-02-19 15:22:15 +0000

    Added a view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,4 +6,13 @@ def book_list(request):
    ## book_info = "\n".join([f"{book.title} by {book.author}" for book in books])
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+from django.views.generic import DetailView
+from .models import Book
+
+class BookDetailView(DetailView):
+      model = Book
+      template_name = 'books/book_detail.html'
+
+
+
 # Create your views here.
