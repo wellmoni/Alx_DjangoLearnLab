0fc35291c3bc8fab9945a485a39fcfeb01070e04
George Monibah <monibahgeorgew@gmail.com>
2025-02-19 15:31:33 +0000

    Added a view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ def book_list(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
