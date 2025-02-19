c2f63b4541fc668acb4b734743ab874aab61aa86
George Monibah <monibahgeorgew@gmail.com>
2025-02-19 10:57:55 +0000

    update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -21,7 +21,9 @@ def list_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     """Retrieve the librarian for a library."""
     try:
+
         library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)s
         return library.librarian.name if library.librarian else "No librarian assigned"
     except Library.DoesNotExist:
         return f"No library found with name '{library_name}'"
