29914769408275d902233294214091cfc800e37f
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 10:28:59 +0000

    completed_task 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,10 +4,21 @@ from . import views
 from .views import BookDetailView 
 from .views import list_books
 from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import RegisterView
+from django.urls import path
+
+##urlpatterns = [
+
+    ##path('book_list/', views.list_books, name='book'),
+   ## path('BookDetailView/', views.BookDetailView.as_view(), name='book_detail'),
+    ##path('admin/', admin.site.urls),
+##]
+
 
 urlpatterns = [
-
-    path('book_list/', views.list_books, name='book'),
-    path('BookDetailView/', views.BookDetailView.as_view(), name='book_detail'),
-    path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='registration_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration_app/logout.html'), name='logout')
+    path('register/', RegisterView.as_view(template_name='registration_app/register.html'), name='register')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,8 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from .models import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView, RegisterView
+from django.urls import path 
 
 
 class BookDetailView(DetailView):
@@ -19,7 +21,11 @@ class BookDetailView(DetailView):
       context_object_name = 'book'
 
 ##return render (request, 'relationship_app/library_detail.html', "library",)
-
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(template_name='registration_app/register.html'),name='register')
+]
 
 
 # Create your views here.
