0bcafbcabebabf5e3980fa9151e406425245149e
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 10:54:04 +0000

    updates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,11 +22,11 @@ class BookDetailView(DetailView):
       context_object_name = 'book'
 
 ##return render (request, 'relationship_app/library_detail.html', "library",)
-urlpatterns = [
-    path('login/', LoginView.as_view(template_name='registration_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(template_name='registration_app/register.html'),name='register')
-]
+class registerView(UserCreationForm):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration_app/register.html'
+
 
 
 # Create your views here.
