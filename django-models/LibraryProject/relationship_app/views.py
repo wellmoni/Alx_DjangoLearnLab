b74f99b0d558d2b464d7d1652ecfda179f6d73ca
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 11:05:59 +0000

    updates_1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -25,7 +25,7 @@ class BookDetailView(DetailView):
 class register(UserCreationForm):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration_app/register.html'
+    template_name = 'relationship_app/register.html'
 
 
 
