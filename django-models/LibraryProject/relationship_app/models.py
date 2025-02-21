9e64e930ef91b3202097f9b2b7fb6457fa1a566f
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 11:37:24 +0000

    Models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -26,4 +26,12 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE )
 
+class UserProfile(models.Model):
+    user = models.OneToOneField ( on_delete=models.CASCADE)
+    role = models.CharField ('Admin','Librarian', 'Member')
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
 # Create your models here.
