b824ff618dba2f2f5c8dfd9c79014c266d71dcba
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 17:16:22 +0000

    added another

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -38,4 +38,7 @@ urlpatterns = [
     path('books/add/', add_book, name='add_book'),
     path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
     path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+
+    path('add_book', add_book, name='add_book')
+    path('edit_book',edit_book, name ='edit_book'),
 ]
