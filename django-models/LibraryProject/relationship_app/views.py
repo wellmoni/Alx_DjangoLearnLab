3656b29c3577890a172c7264ab63a711be7576b6
George Monibah <monibahgeorgew@gmail.com>
2025-02-21 16:19:00 +0000

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 def is_admin(user):
     return user.userprofile.role =='Admin'
 def is_librarian(user):
-    return user.userprofile.role =='librarian'
+    return user.userprofile.role =='Librarian'
 def is_member(user):
-    return user.userprofile.role =='member'
+    return user.userprofile.role =='Member'
 
 
 @user_passes_test(is_admin)
@@ -15,9 +15,9 @@ def admin_view(request):
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian.html', {'role': 'librarian'})
+    return render(request, 'relationship_app/librarian.html', {'role': 'Librarian'})
 @user_passes_test(is_admin)
 
 def member_view(request):
-    return render(request, 'relationship_app/member.html', {'role': 'member'})
+    return render(request, 'relationship_app/member.html', {'role': 'Member'})
 # Create your views here.
