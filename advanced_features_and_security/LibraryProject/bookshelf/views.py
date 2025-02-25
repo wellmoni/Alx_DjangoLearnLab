3e55af50170bec548f7a6f17eb0d89ee9e4ec78d
George Monibah <monibahgeorgew@gmail.com>
2025-02-25 15:46:24 +0000

    task 1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+@permission_required('book_list', raise_exception= books)
+
 
 # Create your views here.
