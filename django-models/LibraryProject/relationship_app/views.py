5244c819f83f5e08cbf1beaa47e372206e071559
George Monibah <monibahgeorgew@gmail.com>
2025-02-25 09:57:28 +0000

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,27 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
 
+from django.shortcuts import render
+
+def list_books(request):
+    books = Book.objects.all()
+    
+   ## book_info = "\n".join([f"{book.title} by {book.author}" for book in books])
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+from .models import LibraryDetailView
+
+
+class BookDetailView(DetailView):
+      model = Book
+      template_name = 'relationship_app/list_books.html'
+      context_object_name = 'book'
+
+
 def is_admin(user):
     return user.userprofile.role =='Admin'
 def is_librarian(user):
