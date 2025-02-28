abf003da7d32e02456f1b60b5bef0a0bf510c7d1
George Monibah <monibahgeorgew@gmail.com>
2025-02-28 10:40:42 +0000

    Task 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from .forms import ExampleForm
 
 @permission_required('book_list', raise_exception= books)
 
