599b519b465c848efac63d84de93e0504b6729b2
George Monibah <monibahgeorgew@gmail.com>
2025-03-06 10:11:33 +0000

    Task 1 Api_project

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from .views import BookList
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
 ]
