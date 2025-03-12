fd79684f355c5f2db59377a1a6585daed3d1b702
wellmoni <wellmoni95@gmail.com>
2025-03-12 13:50:43 +0000

    Added a urlsm

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListView, DetailView
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
 ]
