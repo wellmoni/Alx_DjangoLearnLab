384914aa4c2e2abd1619d3a5b38b2732ad5e0039
wellmoni <wellmoni95@gmail.com>
2025-03-12 14:07:29 +0000

    Added a view_2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -28,4 +28,24 @@ class DeleteView(generics.DestroyAPIView):  # Deletes a book
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    # Add Filtering, Searching, and Ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+
+
+
 # Create your views here.
