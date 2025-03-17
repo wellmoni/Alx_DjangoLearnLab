6f16989bce645599ea51b32d9631e5fe7e51bf44
George Monibah <monibahgeorgew@gmail.com>
2025-03-17 14:59:02 +0000

    Added a blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+##DATABASES = {
+  ##  'default': {
+    ##    'ENGINE': 'django.db.backends.sqlite3',
+      ##  'NAME': BASE_DIR / 'db.sqlite3',
+    ##}
+##}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "your_database_name",
+        "USER": "your_database_user",
+        "PASSWORD": "your_database_password",
+        "HOST": "localhost",
+        "PORT": "3306",  # Default MySQL port
     }
 }
+
 
 
 # Password validation
