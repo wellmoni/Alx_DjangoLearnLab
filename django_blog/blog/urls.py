569a4617c5cf4ebbe9c7279541e744841f238b94
George Monibah <monibahgeorgew@gmail.com>
2025-03-17 15:50:50 +0000

    Added a blog_Task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,8 +19,8 @@ from .views import (
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post_create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
