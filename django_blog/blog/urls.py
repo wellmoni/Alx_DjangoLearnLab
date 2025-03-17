8d5160c2716b872cccaeb878d1fe394c2f18207c
George Monibah <monibahgeorgew@gmail.com>
2025-03-17 16:01:35 +0000

    Added a blog_Task 3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,7 +31,7 @@ from django.urls import path
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
